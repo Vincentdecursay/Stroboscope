8e71201b2166d6992da13aedef02c407a7b1e215
Vincentdecursay <32644216+Vincentdecursay@users.noreply.github.com>
2017-11-22 19:56:49 +0100

    V1.0
    1 DEL
    1 potentiomètre 10k

---
+++ Stroboscope LED 10k potentiometer.ino
@@ -0,0 +1,15 @@
+int ledPin = 13; 
+void setup() {
+pinMode(ledPin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+   int sensorValue = analogRead(A0)/10; //10K potentiometer analog pin 0
+  
+  digitalWrite(ledPin, HIGH);
+  delay(sensorValue);
+  Serial.println(sensorValue);
+  digitalWrite(ledPin, LOW);
+  delay(1);
+}
